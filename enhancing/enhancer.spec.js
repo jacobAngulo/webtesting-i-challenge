b59e39df977f23bba070d9077d01102b4fab8d6b
Jacob Angulo <jacobangulo@hotmail.com>
2019-04-08 15:30:45 -0700

    mvp

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -2,9 +2,54 @@ const enhancer = require("./enhancer.js");
 // test away!
 describe("enhancer.js", () => {
   describe("enhancer", () => {
-    describe("succeed", () => {});
+    describe("succeed", () => {
+      it(
+        "it should increase the items enhancement by 1 if the enhancement level is under 20",
+        () => {
+          expect(
+            enhancer.succeed({
+              name: "sword of a thousand truths",
+              enhancement: 7,
+              durability: 67
+            })
+          ).toEqual({
+            name: "sword of a thousand truths",
+            enhancement: 8,
+            durability: 67
+          });
+        }
+      );
+    });
     describe("fail", () => {});
-    describe("repair", () => {});
-    describe("get", () => {});
+    describe("repair", () => {
+      it("should receive an item and return it with it's durabilty set to 100", () => {
+        expect(
+          enhancer.repair({
+            name: "sword of a thousand truths",
+            enhancement: 7,
+            durability: 67
+          })
+        ).toEqual({
+          name: "sword of a thousand truths",
+          enhancement: 7,
+          durability: 100
+        });
+      });
+    });
+    describe("get", () => {
+      it("should decrese an items durabilty by 5 if the enhancement level is lower than 15, 10 if it is greater, and if the enhancement level is over 16 the enhancement level should be decreased by 1", () => {
+        expect(
+          enhancer.fail({
+            name: "sword of a thousand truths",
+            enhancement: 7,
+            durability: 67
+          })
+        ).toEqual({
+          name: "sword of a thousand truths",
+          enhancement: 7,
+          durability: 62
+        });
+      });
+    });
   });
 });
